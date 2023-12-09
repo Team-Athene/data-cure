305d938bbfb4829b685bf88717745868408fa49a
mekhakrishnanm <51077062+mekhakrishnanm@users.noreply.github.com>
2023-12-09 22:15:31 +0530

    Verificat
    feat(deployment): deploy and verify DataCureAccess contract
    
    Deployed the DataCureAccess smart contract, extending deployment script to include its deployment and verification. Removed unused import `formatEther`, `parseEther` from `viem`. Refactored DataCure deployment arguments into a separate variable for consistency and potential reusability. Added verification steps for Verifier, DataCure, and DataCureAccess contracts after deployment, ensuring all deployed contracts are verified on the network

--- smart-contract/scripts/deploy.ts
+++ smart-contract/scripts/deploy.ts
@@ -1,17 +1,49 @@
-import { formatEther, parseEther } from "viem";
 import hre from "hardhat";
+import { run } from "hardhat";
 
 async function main() {
 
   const verifier = await hre.viem.deployContract("Verifier");
-  const dataCure = await hre.viem.deployContract("DataCure", [verifier.address]);
   console.log(
     `Verifier deployed to ${verifier.address}`
   );
 
+  const dataCureArgs = [verifier.address]
+  const dataCure = await hre.viem.deployContract("DataCure", dataCureArgs);
   console.log(
     `DataCure deployed to ${dataCure.address}`
   );
+
+  const dataCureAccessArgs = [dataCure.address]
+  const dataCureAccess = await hre.viem.deployContract("DataCureAccess", dataCureAccessArgs);
+  console.log(
+    `DataCureAccess deployed to ${dataCureAccess.address}`
+  );
+
+  console.log("🚀 All contracts have been deployed");
+
+    //Verify Verifier contract
+    await run("verify:verify", {
+      address: verifier.address,
+      constructorArguments: [],
+    });
+    console.log("Verifier verified");
+
+
+    //Verify DataCure contract
+    await run("verify:verify", {
+      address: dataCure.address,
+      constructorArguments: dataCureArgs,
+    });
+    console.log("DataCure verified");
+
+    //Verify DataCureAccess contract
+    await run("verify:verify", {
+      address: dataCureAccess.address,
+      constructorArguments: dataCureAccessArgs,
+    });
+    console.log("DataCureAccess verified");
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
