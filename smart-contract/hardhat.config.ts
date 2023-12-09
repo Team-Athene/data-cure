446adb5b357ca7f25a6e2ee4b8c16f9406b11b04
Anas KVM <kvmanas@gmail.com>
2023-12-09 15:18:06 +0530

    feat(smart-contract): deploy DataCure contract

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
+  defaultNetwork: "polygon_mumbai",
+  networks: {
+    hardhat: {
+    },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY as string]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
 };
 
 export default config;
