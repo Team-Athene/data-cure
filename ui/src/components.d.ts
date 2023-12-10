50a57c70168330ab6294cc0a1a7f6530cff7085c
Anas KVM <kvmanas@gmail.com>
2023-12-10 11:23:44 +0530

    fix

--- ui/src/components.d.ts
+++ ui/src/components.d.ts
@@ -9,12 +9,11 @@ declare module 'vue' {
   export interface GlobalComponents {
     AAlert: typeof import('anu-vue')['AAlert']
     ABtn: typeof import('anu-vue')['ABtn']
-    ADialog: typeof import('anu-vue')['ADialog']
+    ACard: typeof import('anu-vue')['ACard']
     AInput: typeof import('anu-vue')['AInput']
     AList: typeof import('anu-vue')['AList']
     ALoadingIcon: typeof import('anu-vue')['ALoadingIcon']
     AMenu: typeof import('anu-vue')['AMenu']
-    ARadio: typeof import('anu-vue')['ARadio']
     ASelect: typeof import('anu-vue')['ASelect']
     FileForm: typeof import('./components/FileForm.vue')['default']
     FileInput: typeof import('./components/FileInput.vue')['default']
